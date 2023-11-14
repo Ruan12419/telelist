12ca5ffcf05df5dc92c4b1df587cac841332dbcf
Ruan Lima <az.88004607@gmail.com>
2023-11-14 01:49:00 -0300

    Estilização do layout da tela de login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -20,69 +20,108 @@ class _LoginState extends State<Login> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
+        backgroundColor: Colors.grey,
       ),
+      backgroundColor: Colors.grey[350],
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                initialValue: user.username,
-                decoration: const InputDecoration(
-                  labelText: 'Usuário',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira seu nome de usuário';
-                  }
-                  return null;
-                },
-                onSaved: (value) => user.setUsername(value!),
-              ),
-              TextFormField(
-                initialValue: user.password,
-                decoration: const InputDecoration(
-                  labelText: 'Senha',
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira sua senha';
-                  }
-                  return null;
-                },
-                onSaved: (value) => user.setPassword(value!),
-              ),
-              ElevatedButton(
-                child: const Text('Entrar'),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    if (user.isUserRegistered(user.username, user.password)) {
-                      Navigator.pushReplacement(
+        child: Column(
+          children: [
+            Padding(padding: const EdgeInsets.all(20)),
+            Image.asset("imagens/logo.jpeg"),
+            Padding(padding: const EdgeInsets.all(20)),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    initialValue: user.username,
+                    decoration: const InputDecoration(
+                      fillColor: Color.fromARGB(255, 238, 238, 238),
+                      filled: true,
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.all(Radius.elliptical(12.0, 12.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira seu nome de usuário';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => user.setUsername(value!),
+                  ),
+                  Padding(padding: const EdgeInsets.all(5)),
+                  TextFormField(
+                    initialValue: user.password,
+                    decoration: const InputDecoration(
+                      fillColor: Color.fromARGB(255, 238, 238, 238),
+                      filled: true,
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      border: OutlineInputBorder(
+                        borderRadius:
+                            BorderRadius.all(Radius.elliptical(12.0, 12.0)),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, insira sua senha';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => user.setPassword(value!),
+                  ),
+                  Padding(padding: const EdgeInsets.all(20)),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    child: const Text('Entrar'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        if (user.isUserRegistered(
+                            user.username, user.password)) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Home()),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Usuário não registrado')),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                  Padding(padding: const EdgeInsets.all(5)),
+                  TextButton(
+                    child: const Text(
+                      'Registrar',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => const Home()),
+                        MaterialPageRoute(
+                            builder: (context) => const Register()),
                       );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Usuário não registrado')),
-                      );
-                    }
-                  }
-                },
+                    },
+                  ),
+                ],
               ),
-              TextButton(
-                child: const Text('Registrar'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Register()),
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
