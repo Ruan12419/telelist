2de75a702b55ed9ca1d114a298e08f9749b71eb9
Ruan Lima <az.88004607@gmail.com>
2023-11-29 22:38:51 -0300

    Criação tela movies info

---
+++ lib/screens/movies_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesInfo extends StatefulWidget {
+  const MoviesInfo({super.key});
+
+  @override
+  State<MoviesInfo> createState() => _MoviesInfoState();
+}
+
+class _MoviesInfoState extends State<MoviesInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
